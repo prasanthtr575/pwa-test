54a58c204769c74d2389ba832324e4f0d8a4cf32
Prasanth T R <mailprasanthtr@gmail.com>
2018-03-31 21:14:31 +0530

    set up .travis.yml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 }
+
+
+// 1/SVLX80Wc3C1EVvXXVmXeqq35KTn9SiQ-aFTyjjCn2v2_jEDpptYSjfqODViuFTrZ
+
+// FIREBASE_TOKEN
